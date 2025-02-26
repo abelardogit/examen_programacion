76f82aec60387599e63c902c7974f852ac7031be
MiUsuario <github@abelardolg.com>
2025-02-26 12:11:34 +0100

    Fix wrong code

--- src/reto1/NIEApp.java
+++ src/reto1/NIEApp.java
@@ -10,8 +10,8 @@ public class NIEApp {
 
     private static void testNIE() {
         boolean isNIE1Valid = Validator.isNIEValid(NIETest.NIE_VALIDO);
-        boolean isNIE2NotValid = Validator.isNIEValid(NIETest.NIE_NO_VALIDO);
-        if (isNIE1Valid && isNIE2NotValid) {
+        boolean isNIE2Valid = Validator.isNIEValid(NIETest.NIE_NO_VALIDO);
+        if (isNIE1Valid && !isNIE2Valid) {
             Output.passedTest();
         } else {
             Output.failedTest();
