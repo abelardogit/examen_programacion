29e0c2abbae01f6ec2fb02d39f01c0802230589a
MiUsuario <github@abelardolg.com>
2025-02-26 11:43:31 +0100

    Add extra information

--- src/reto1/Validator.java
+++ src/reto1/Validator.java
@@ -5,7 +5,7 @@ package reto1;
 
 public class Validator {
     public static boolean isNIEValid(String aNIE) {
-        // Implementar la validación del NIE
+        // Implementar la validación del NIE SIN usar la ayuda de JAVA.
         return true; // Devolved el valor que consideréis
     }
 }
