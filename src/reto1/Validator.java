dc1a194d51044d10ef1efa437e9c9c142b9c80c3
MiUsuario <github@abelardolg.com>
2025-02-26 11:52:33 +0100

    Added the steps of the NIE validation.

--- src/reto1/Validator.java
+++ src/reto1/Validator.java
@@ -5,7 +5,14 @@ package reto1;
 
 public class Validator {
     public static boolean isNIEValid(String aNIE) {
-        // Implementar la validación del NIE SIN usar la ayuda de JAVA.
+        // 0. Formato del NIE (no tienes que validarlo): "A9999999B" (1 letra + 7 dígitos + 1 letra)
+        // 1. 'A' solo puede ser 'X', 'Y' o 'Z' (no tienes que validarlo).
+        // 2. Sustituye su valor. Usa el método NIEDB.getValueFirstLetter para ello.
+        // 3. Convierte la cadena numérica a un número. ❌ No uses JAVA en este paso.
+        // 4. Halla el resto de este número con 23.
+        // 5. Usa el método NIEDB.getLetter para saber si la letra del NIE proporcionado es correcta.
+
+
         return true; // Devolved el valor que consideréis
     }
 }
